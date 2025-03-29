5d92679d814bf251c215b5213fb2c56779a78d32
Nagraj Nandal <nandalnagraj2308@gmail.com>
2025-03-29 11:18:39 +0530

    Update intro-section.jsx linkdin changes

--- src/app/components/intro-section.jsx
+++ src/app/components/intro-section.jsx
@@ -62,7 +62,7 @@ export default function IntroSection() {
             <a href="https://github.com/Nagraj23" className="text-gray-400 hover:text-white transition-colors">
               <FaGithub size={24} />
             </a>
-            <a href="www.linkedin.com/in/nagraj-nandal-71a361333" className="text-gray-400 hover:text-white transition-colors">
+            <a href="https://www.linkedin.com/in/nagraj-nandal-71a361333/" className="text-gray-400 hover:text-white transition-colors">
               <FaLinkedin size={24} />
             </a>
             <a href="https://www.instagram.com/nagraj_np2308/" className="text-gray-400 hover:text-white transition-colors">
